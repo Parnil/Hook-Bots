a9fab3f01bbba741882795fc5eeb8b9846e1cfae
Peter Tousioudis <parnil.tarmikos@gmail.com>
2017-11-09 13:26:27 +0200

    Basic Game Setup
    Done with basic Scene Setup and Player Controller basics(movement, jumping).

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private float jumpThrust = 10f;
+	private float speed = 5f;
+	private Rigidbody playerRB;
+	private bool jumping = false;
+
+	// Use this for initialization
+	void Start () {
+		playerRB = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Movement();
+
+		if (Input.GetKeyDown(KeyCode.Space) && !jumping)
+		{
+			jumping = true;
+			Jump();
+		}
+	}
+
+	void Movement()
+	{
+		float movement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+		transform.Translate(movement, 0, 0);
+	}
+
+	void Jump()
+	{
+		playerRB.AddForce(0, jumpThrust, 0, ForceMode.Impulse);
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.tag == "Respawn")
+		{
+			transform.SetPositionAndRotation(new Vector3(0, 2, 0), Quaternion.identity);
+		}
+
+		if(collision.gameObject.tag == "Ground")
+		{
+			jumping = false;
+		}
+	}
+}
